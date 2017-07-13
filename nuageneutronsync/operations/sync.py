a80e8a8df4783d82754f02792361170252ba9e67
dieterdm <dieter.de.moitie@gmail.com>
2017-07-13 15:54:06 +0200

    Add IPv6

--- nuageneutronsync/operations/sync.py
+++ nuageneutronsync/operations/sync.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
 import MySQLdb as mdb
-import vspk.v4_0 as vspk
 import logging
+import sys
+import importlib
 from keystoneauth1 import session
 from keystoneauth1.identity import v3 as identity
 from keystoneclient.v3 import client as keystone_client
 from neutronclient.v2_0 import client as neutron_client
 from netaddr import IPAddress, IPNetwork, AddrFormatError
 
+vspk = None
 
 class Sync:
     def __init__(self, config):
         self.cfg = config
         self.logger = logging.getLogger('nuage-neutron-sync')
+
+        self.version = self.cfg.get_value('vsd', 'version')
+        try:
+            global vspk
+            vspk = importlib.import_module('vspk.{0:s}'.format(self.version))
+        except ImportError as e:
+            self.logger.error("Invalid VSPK version")
+            self.logger.error(str(e))
+            sys.exit('Invalid VSPK version')
 
         # Initialising
         self.neutron = None
@@ -54,9 +65,11 @@ class Sync:
                     tenants_to_sync.append((ent, project))
         return tenants_to_sync
 
+    # Returns dictionary with key is the Nuage subnet id and value is a list of tuples containing
+    # (openstack subnet id and ip 4/6 version)
     def get_current_subnet_mappings(self, net_partition_id):
-        query = "SELECT subnet_id, nuage_subnet_id FROM nuage_subnet_l2dom_mapping where net_partition_id = '{0}'" \
-            .format(net_partition_id)
+        query = "SELECT subnet_id, nuage_subnet_id, ip_version FROM nuage_subnet_l2dom_mapping where " \
+                "net_partition_id = '{0}'".format(net_partition_id)
 
         cur = self.mysql_con.cursor(mdb.cursors.DictCursor)
         cur.execute(query)
@@ -64,10 +77,13 @@ class Sync:
         self.mysql_con.commit()  # commit to avoid caching the result of this query
 
         # The mysql result is a tuple of dictionaries
-        # Convert this to one dictionary with key=nuage_subnet_id and value=subnet_id
+        # Convert this to one dictionary with key=nuage_subnet_id and value=list of tuples (subnet_id, ip_version)
         mappings = {}
         for subnet in rows:
-            mappings[subnet['nuage_subnet_id']] = subnet['subnet_id']
+            # if not defined, set the key to []
+            mappings.setdefault(subnet['nuage_subnet_id'], [])
+            # add tuple
+            mappings[subnet['nuage_subnet_id']].append((subnet['subnet_id'], subnet['ip_version']))
         return mappings
 
     def calc_subnetname(self, nuage_subnet):
@@ -100,7 +116,13 @@ class Sync:
         return "{0}/{1}".format(net, str(IPAddress(nm).netmask_bits()))
 
     # calculate address, netmask and gateway to be used
-    def get_cidr_and_gateway(self, nuage_subnet):
+    def get_cidr_and_gateway(self, nuage_subnet, ip_version):
+        if ip_version == 4:
+            return self.get_cidr_and_gateway_v4(nuage_subnet)
+        else:
+            return self.get_cidr_and_gateway_v6(nuage_subnet)
+
+    def get_cidr_and_gateway_v4(self, nuage_subnet):
         if nuage_subnet.associated_shared_network_resource_id is not None:
             # shared subnet
             shared_network = vspk.NUSharedNetworkResource(id=nuage_subnet.associated_shared_network_resource_id)
@@ -161,7 +183,20 @@ class Sync:
                     self.logger.error(str(e))
         return cidr, gateway, enable_dhcp
 
-    def add_neutron_subnet(self, nuage_subnet, project, enterprise):
+    def get_cidr_and_gateway_v6(self, nuage_subnet):
+        if nuage_subnet.parent_type != "enterprise":
+            # L3 subnet
+            gateway = nuage_subnet.ipv6_gateway
+        else:
+            # L2 subnet
+            gateway = None
+        # DHCPv6 not yet supported in OpenStack
+        enable_dhcp = False
+        cidr = nuage_subnet.ipv6_address
+
+        return cidr, gateway, enable_dhcp
+
+    def add_neutron_network(self, nuage_subnet, project, enterprise):
         net_name = self.calc_subnetname(nuage_subnet)
 
         # create network
@@ -181,14 +216,21 @@ class Sync:
             self.logger.error(str(e))
             return
 
-        cidr, gateway, enable_dhcp = self.get_cidr_and_gateway(nuage_subnet)
+        if nuage_subnet.ip_type != "IPV6":
+            # IPV4, DUALSTACK or Null
+            self.add_neutron_subnet(enterprise, 4, net_name, network, nuage_subnet, project)
 
+        if nuage_subnet.ip_type == "DUALSTACK" or nuage_subnet.ip_type == "IPV6":
+            self.add_neutron_subnet(enterprise, 6, net_name, network, nuage_subnet, project)
+
+    def add_neutron_subnet(self, enterprise, ip_version, net_name, network, nuage_subnet, project):
+        cidr, gateway, enable_dhcp = self.get_cidr_and_gateway(nuage_subnet, ip_version)
         body_subnet = {
             "subnets": [
                 {
                     "name": net_name,
                     "cidr": cidr,
-                    "ip_version": 4,
+                    "ip_version": ip_version,
                     "network_id": network['network']['id'],
                     "nuagenet": nuage_subnet.id,
                     "net_partition": enterprise.name,
@@ -196,12 +238,9 @@ class Sync:
                 }
             ]
         }
-
         body_subnet['subnets'][0]['enable_dhcp'] = enable_dhcp
-
         if gateway is not None:
             body_subnet['subnets'][0]['gateway_ip'] = gateway
-
         try:
             self.logger.debug("Creating subnet with body {0}".format(body_subnet))
             self.neutron.create_subnet(body=body_subnet)
@@ -211,41 +250,65 @@ class Sync:
             self.logger.error(str(e))
             self.neutron.delete_network(network['network']['id'])
 
-    def del_neutron_network(self, openstack_subnet_id):
+    def del_neutron_network(self, openstack_subnets):
+        # get the OpenStack subnet id of one of the (ipv4, ipv6) subnets
+        openstack_subnet_id = openstack_subnets[0][0]
         subnet = self.neutron.show_subnet(openstack_subnet_id)['subnet']
-        # Subnet will be removed if network is removed
+        # All (ipv4/ipv6) subnets will be removed if network is removed
         self.neutron.delete_network(subnet['network_id'])
 
-    def check_if_correct(self, nuage_subnet, openstack_subnet_id):
+    def check_if_correct(self, nuage_subnet, openstack_subnets):
         if nuage_subnet.parent_type != "enterprise" and nuage_subnet.address is None \
                 and nuage_subnet.associated_shared_network_resource_id is None:
             self.logger.debug("VSD subnet {0} is a shared L3 subnet without the subnet attached. Ignoring."
                               .format(nuage_subnet.id))
             return False
 
+        ip_versions = []
+        for openstack_subnet in openstack_subnets:
+            ip_versions.append(openstack_subnet[1])
+
+        if nuage_subnet.ip_type == "IPV6":
+            if 6 not in ip_versions or 4 in ip_versions:
+                self.logger.debug("IP type of Nuage subnet {0} is not same as subnets found in OpenStack {1}"
+                                  .format(nuage_subnet.id, openstack_subnets))
+                return False
+        elif nuage_subnet.ip_type == "IPV4" or nuage_subnet.ip_type is None:
+            if 6 in ip_versions or 4 not in ip_versions:
+                self.logger.debug("IP type of Nuage subnet {0} is not same as subnets found in OpenStack {1}"
+                                  .format(nuage_subnet.id, openstack_subnets))
+                return False
+        elif nuage_subnet.ip_type == "DUALSTACK":
+            if 6 not in ip_versions or 4 not in ip_versions:
+                self.logger.debug("IP type of Nuage subnet {0} is not same as subnets found in OpenStack {1}"
+                                  .format(nuage_subnet.id, openstack_subnets))
+                return False
+
         mapping_correct = True
-        openstack_subnet = self.neutron.show_subnet(openstack_subnet_id)['subnet']
+        for openstack_subnet_id, ip_version in openstack_subnets:
+            openstack_subnet = self.neutron.show_subnet(openstack_subnet_id)['subnet']
 
-        subnet_name = self.calc_subnetname(nuage_subnet)
-        if subnet_name != openstack_subnet['name']:
-            mapping_correct = False
-            self.logger.debug("VSD subnet {0} was renamed from \"{0}\" to \"{1}\"".format(nuage_subnet.id,
-                                                                                          openstack_subnet['name'],
-                                                                                          subnet_name))
+            subnet_name = self.calc_subnetname(nuage_subnet)
+            if subnet_name != openstack_subnet['name']:
+                mapping_correct = False
+                self.logger.debug("VSD subnet {0} was renamed from \"{0}\" to \"{1}\"".format(nuage_subnet.id,
+                                                                                              openstack_subnet['name'],
+                                                                                              subnet_name))
 
-        cidr, gateway, enable_dhcp = self.get_cidr_and_gateway(nuage_subnet)
-        if enable_dhcp != openstack_subnet['enable_dhcp']:
-            mapping_correct = False
-            self.logger.debug("DHCP not correct in subnet {0} (\"{1}\")".format(nuage_subnet.id, subnet_name))
+            cidr, gateway, enable_dhcp = self.get_cidr_and_gateway(nuage_subnet, ip_version)
+            if enable_dhcp != openstack_subnet['enable_dhcp']:
+                mapping_correct = False
+                self.logger.debug("DHCP not correct in subnet {0} (\"{1}\")".format(nuage_subnet.id, subnet_name))
 
-        if openstack_subnet['gateway_ip'] != gateway:
-            mapping_correct = False
-            self.logger.debug("Gateway IP of subnet {0} (\"{1}\") changed to {2}".
-                              format(nuage_subnet.id, subnet_name, gateway))
+            if openstack_subnet['gateway_ip'] != gateway:
+                mapping_correct = False
+                self.logger.debug("Gateway IP of subnet {0} (\"{1}\") changed to {2}"
+                                  .format(nuage_subnet.id, subnet_name, gateway))
 
-        if cidr != openstack_subnet['cidr']:
-            self.logger.debug("CIDR of subnet {0} (\"{1}\") changed to {2}".format(nuage_subnet.id, subnet_name, cidr))
-            mapping_correct = False
+            if IPNetwork(cidr) != IPNetwork(openstack_subnet['cidr']):
+                self.logger.debug("CIDR of subnet {0} (\"{1}\") changed to {2}"
+                                  .format(nuage_subnet.id, subnet_name, cidr))
+                mapping_correct = False
 
         return mapping_correct
 
@@ -265,13 +328,13 @@ class Sync:
             self.sync_subnet(enterprise, nuage_subnet, project, subnet_mappings)
 
         # all remaining subnet mappings do not exist in VSD anymore, delete neutron subnets
-        for openstack_subnet_id in subnet_mappings.itervalues():
-            self.logger.info("OpenStack subnet {0} does not exist anymore in VSD. Deleting..."
-                             .format(openstack_subnet_id))
+        for openstack_subnets in subnet_mappings.itervalues():
+            self.logger.info("OpenStack subnets {0} do not exist anymore in VSD. Deleting..."
+                             .format(openstack_subnets))
             try:
-                self.del_neutron_network(openstack_subnet_id)
+                self.del_neutron_network(openstack_subnets)
             except Exception as e:
-                self.logger.error("Unable to delete network {0}".format(openstack_subnet_id))
+                self.logger.error("Unable to delete networks {0}".format(openstack_subnets))
                 self.logger.error(str(e))
 
     def sync_subnet(self, enterprise, nuage_subnet, project, subnet_mappings):
@@ -297,21 +360,21 @@ class Sync:
             synced = True
             if self.check_if_correct(nuage_subnet, subnet_mappings[nuage_subnet.id]) is False:
                 try:
-                    self.logger.info("VSD subnet {0} has changed. Deleting mapped OpenStack subnet {1}"
+                    self.logger.info("VSD subnet {0} has changed. Deleting mapped OpenStack subnet(s) {1}"
                                      .format(nuage_subnet.id, subnet_mappings[nuage_subnet.id]))
                     self.del_neutron_network(subnet_mappings[nuage_subnet.id])
                 except Exception as e:
-                    self.logger.error("Unable to delete OpenStack subnet {0}"
+                    self.logger.error("Unable to delete OpenStack subnet(s) {0}"
                                       .format(subnet_mappings[nuage_subnet.id]))
                     self.logger.error(str(e))
                 else:
                     synced = False
             else:
-                self.logger.debug("VSD subnet {0} is correctly mapped to OpenStack subnet {1}".
+                self.logger.debug("VSD subnet {0} is correctly mapped to OpenStack subnet(s) {1}".
                                   format(nuage_subnet.id, subnet_mappings[nuage_subnet.id]))
             # mapping is either correct or not relevant anymore, delete item from dictionary
             # all remaining items in subnet_mappings will be deleted
             del subnet_mappings[nuage_subnet.id]
         if synced is False:
             # Create network in OpenStack
-            self.add_neutron_subnet(nuage_subnet, project, enterprise)
+            self.add_neutron_network(nuage_subnet, project, enterprise)
